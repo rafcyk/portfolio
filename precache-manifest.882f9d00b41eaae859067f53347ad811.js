1cba931959555ab29ea88cddcce6283e7c3a7c08
Rafcyk <rafcy9@gmail.com>
2020-06-25 04:36:32 +0200

    Updates

---
+++ precache-manifest.882f9d00b41eaae859067f53347ad811.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4ce902d6456928cea3ae2a77e7d4ba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "455ee3e075e3271ca07a",
+    "url": "/portfolio/static/css/main.ff1b9fcd.chunk.css"
+  },
+  {
+    "revision": "53a1a898128c54dc8fb2",
+    "url": "/portfolio/static/js/2.b806b714.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b806b714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455ee3e075e3271ca07a",
+    "url": "/portfolio/static/js/main.ec4366b0.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "8ce3b1364d44db7bf1dc7799c5497f5b",
+    "url": "/portfolio/static/media/CV_Rafał_Dobrowolski.8ce3b136.pdf"
+  },
+  {
+    "revision": "b39b2629786012cc249db2f11428e149",
+    "url": "/portfolio/static/media/DjWebsite.b39b2629.png"
+  },
+  {
+    "revision": "c77ac5e4b9264f5b4bedc780f247a06b",
+    "url": "/portfolio/static/media/about.c77ac5e4.png"
+  },
+  {
+    "revision": "1e34c98f559ef40199e93d10be91c70f",
+    "url": "/portfolio/static/media/bgc-header.1e34c98f.png"
+  },
+  {
+    "revision": "7c76ad8e6b6f29355cff0f418e2b6dd5",
+    "url": "/portfolio/static/media/contact.7c76ad8e.jpg"
+  },
+  {
+    "revision": "43daee5bcc0812684a8b0badf2662340",
+    "url": "/portfolio/static/media/curiosities.43daee5b.png"
+  },
+  {
+    "revision": "ed7dc8e8d266ef7d58736a9f3aca3b16",
+    "url": "/portfolio/static/media/find-image.ed7dc8e8.png"
+  },
+  {
+    "revision": "7bbad46c45a4ff6682ddc366a0d47f27",
+    "url": "/portfolio/static/media/quiz.7bbad46c.png"
+  },
+  {
+    "revision": "eb933c05f5b8144531007c5e48d06406",
+    "url": "/portfolio/static/media/react.eb933c05.png"
+  },
+  {
+    "revision": "b3ed558c31b3ce29928fa1d941ecb82f",
+    "url": "/portfolio/static/media/removals.b3ed558c.png"
+  }
+]);
